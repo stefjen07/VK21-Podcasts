925b23adc92906b8ce76e557b717a40f9ce5286a
Eugene stefjen07 <stefjen@mail.ru>
2021-08-11 21:55:21 +0300

    Project initialized

---
+++ VKPodcasts/VKPodcastsApp.swift
@@ -0,0 +1,17 @@
+//
+//  VKPodcastsApp.swift
+//  VKPodcasts
+//
+//  Created by Евгений on 11.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct VKPodcastsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
